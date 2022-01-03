040615a736f9bb104218b29d1b49def31df80181
zjg@0411 <zhoujg0411@163.com>
2022-01-03 21:54:13 +0800

    提交

--- debugtalk.py
+++ debugtalk.py
@@ -13,3 +13,28 @@ def sum_two(m, n):
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+def get_user():
+    return [
+        {"name":"15011110000"},
+        {"name":"15011110001"},
+        {"name":"15011110002"}
+
+    ]
+
+def register_user():
+    user = "test" + str(int(time.time()))
+    return user
+
+def setup_hook():
+    print("---------setup-----------")
+
+def teardown_hook():
+    print("---------teardown-----------")
+
+
+
+if __name__ == "__main__":
+    print(register_user())
+
+
